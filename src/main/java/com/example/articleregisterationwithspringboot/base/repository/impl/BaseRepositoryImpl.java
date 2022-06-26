de2b0fd05d97c5e7830510f7f36ce1f71255ee3c
neda <bashiri.neda72@gmail.com>
2022-06-26 18:24:29 +0430

    implemented BaseRepositoryImpl class

---
+++ src/main/java/com/example/articleregisterationwithspringboot/base/repository/impl/BaseRepositoryImpl.java
@@ -0,0 +1,54 @@
+package com.example.articleregisterationwithspringboot.base.repository.impl;
+
+import com.example.articleregisterationwithspringboot.base.entity.BaseEntity;
+import com.example.articleregisterationwithspringboot.base.repository.BaseRepository;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.io.Serializable;
+
+import java.util.Collection;
+import java.util.List;
+
+public class BaseRepositoryImpl <E extends BaseEntity<PK>,PK extends Serializable> implements BaseRepository<E,PK> {
+
+    EntityManager entityManager;
+
+    private Class<E> clazz;
+
+
+    @Override
+    public E save(E e) {
+        entityManager.persist(e);
+        return e;
+    }
+
+    @Override
+    public E update(E e) {
+        entityManager.merge(e);
+        return e;
+    }
+
+    @Override
+    public E findById(PK id) {
+        return entityManager.find(clazz, id);
+    }
+
+    @Override
+    public List<E> findAll() {
+        TypedQuery<E> query = entityManager.createQuery("select o from"+clazz.getSimpleName()+"o",clazz);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<E> findAllByIdsIn(Collection<PK> ids) {
+
+        return null;
+    }
+
+    @Override
+    public void deleteById(PK id) {
+        entityManager.remove(entityManager.getReference(clazz,id));
+    }
+}
