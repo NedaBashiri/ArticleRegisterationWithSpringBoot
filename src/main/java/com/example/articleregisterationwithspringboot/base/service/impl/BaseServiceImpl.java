6138d04c6159c8f9e136e8341af6639e03aaa027
neda <bashiri.neda72@gmail.com>
2022-06-26 19:36:35 +0430

    implemented BaseServiceImpl class

---
+++ src/main/java/com/example/articleregisterationwithspringboot/base/service/impl/BaseServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.articleregisterationwithspringboot.base.service.impl;
+
+import com.example.articleregisterationwithspringboot.base.entity.BaseEntity;
+import com.example.articleregisterationwithspringboot.base.repository.BaseRepository;
+import com.example.articleregisterationwithspringboot.base.service.BaseService;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+public class BaseServiceImpl<E extends BaseEntity, PK extends Serializable, Repository extends BaseRepository<E, PK>> implements BaseService<E, PK>  {
+
+
+    protected Repository baseRepository;
+
+    public BaseServiceImpl(Repository baseRepository) {
+        this.baseRepository = baseRepository;
+    }
+
+    @Override
+    public E save(E e) {
+        return  baseRepository.save(e);
+    }
+
+    @Override
+    public E update(E e) {
+        return baseRepository.update(e);
+    }
+
+    @Override
+    public E findById(PK id) {
+        return baseRepository.findById(id);
+    }
+
+    @Override
+    public void deleteById(PK id) {
+        baseRepository.deleteById(id);
+    }
+
+    @Override
+    public List<E> findAll() {
+        return baseRepository.findAll();
+    }
+
+    @Override
+    public List<E> findAllByIdsIn(Collection<PK> ids) {
+        return baseRepository.findAllByIdsIn(ids);
+    }
+}
