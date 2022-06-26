7724393803a6a76cb7fec4828084ae68105a3d35
neda <bashiri.neda72@gmail.com>
2022-06-26 17:31:34 +0430

    initial commit

---
+++ src/main/java/com/example/articleregisterationwithspringboot/ArticleRegisterationWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.articleregisterationwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleRegisterationWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleRegisterationWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/articleregisterationwithspringboot/ArticleRegisterationWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.articleregisterationwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleRegisterationWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
