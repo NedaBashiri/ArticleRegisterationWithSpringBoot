8e04860e069bf84944ef2082581d471f7a7b0115
neda <bashiri.neda72@gmail.com>
2022-06-26 17:43:28 +0430

    implemented generic BaseEntity class

---
+++ src/main/java/com/example/articleregisterationwithspringboot/base/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.example.articleregisterationwithspringboot.base.entity;
+
+import java.io.Serializable;
+
+public class BaseEntity<PK extends Serializable> implements Serializable {
+
+    private PK id;
+
+    public void setId(PK id){
+        this.id=id;
+    }
+
+    public PK getId(){
+        return id;
+    }
+}
