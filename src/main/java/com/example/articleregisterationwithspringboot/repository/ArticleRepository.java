b74685bd6b413600ea9a5aafefa6d50ab0ae04ff
neda <bashiri.neda72@gmail.com>
2022-06-27 12:13:47 +0430

    implemented Repository For entities

--- src/main/java/com/example/articleregisterationwithspringboot/repository/ArticleRepository.java
+++ src/main/java/com/example/articleregisterationwithspringboot/repository/ArticleRepository.java
@@ -3,7 +3,11 @@ package com.example.articleregisterationwithspringboot.repository;
 import com.example.articleregisterationwithspringboot.base.repository.BaseRepository;
 import com.example.articleregisterationwithspringboot.domains.Article;
 
+import java.util.List;
+
 public interface ArticleRepository extends BaseRepository<Article,Long> {
 
     Article findByTitle(String title);
+
+    List<Article> findPublishedArticles();
 }
