81c2998c5c00ca119cac3b0050bfae89297298ca
neda <bashiri.neda72@gmail.com>
2022-06-27 12:16:39 +0430

    implemented Service for entities

--- src/main/java/com/example/articleregisterationwithspringboot/service/ArticleService.java
+++ src/main/java/com/example/articleregisterationwithspringboot/service/ArticleService.java
@@ -3,7 +3,11 @@ package com.example.articleregisterationwithspringboot.service;
 import com.example.articleregisterationwithspringboot.base.service.BaseService;
 import com.example.articleregisterationwithspringboot.domains.Article;
 
+import java.util.List;
+
 public interface ArticleService extends BaseService<Article,Long> {
 
     Article findByTitle(String title);
+
+    List<Article> findPublishedArticles();
 }
