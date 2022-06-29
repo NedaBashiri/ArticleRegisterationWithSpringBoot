2ca82e6b588c23648b7590aa24f3db004a6f1d1c
neda <bashiri.neda72@gmail.com>
2022-06-29 12:25:19 +0430

    implemented BaseServiceImpl class

--- src/main/java/com/example/articleregisterationwithspringboot/base/service/impl/BaseServiceImpl.java
+++ src/main/java/com/example/articleregisterationwithspringboot/base/service/impl/BaseServiceImpl.java
@@ -3,14 +3,18 @@ package com.example.articleregisterationwithspringboot.base.service.impl;
 import com.example.articleregisterationwithspringboot.base.entity.BaseEntity;
 import com.example.articleregisterationwithspringboot.base.repository.BaseRepository;
 import com.example.articleregisterationwithspringboot.base.service.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
-
+@Service
+@Transactional
 public class BaseServiceImpl<E extends BaseEntity, PK extends Serializable, Repository extends BaseRepository<E, PK>> implements BaseService<E, PK>  {
 
-
+    @Autowired
     protected Repository baseRepository;
 
 
