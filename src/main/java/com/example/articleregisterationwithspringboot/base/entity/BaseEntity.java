17f32ff6f4be41df4e0d4f4198562e5d82030e76
neda <bashiri.neda72@gmail.com>
2022-06-29 12:19:19 +0430

    implemented generic BaseEntity class

--- src/main/java/com/example/articleregisterationwithspringboot/base/entity/BaseEntity.java
+++ src/main/java/com/example/articleregisterationwithspringboot/base/entity/BaseEntity.java
@@ -1,9 +1,16 @@
 package com.example.articleregisterationwithspringboot.base.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity<PK extends Serializable> implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private PK id;
 
     public void setId(PK id){
