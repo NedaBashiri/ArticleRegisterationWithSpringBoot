459e4c767d9fac7fdbfce0edcf447f534c4d1ac9
neda <bashiri.neda72@gmail.com>
2022-06-29 12:32:14 +0430

    initial commit

--- src/main/java/com/example/articleregisterationwithspringboot/ArticleRegisterationWithSpringBootApplication.java
+++ src/main/java/com/example/articleregisterationwithspringboot/ArticleRegisterationWithSpringBootApplication.java
@@ -1,12 +1,14 @@
 package com.example.articleregisterationwithspringboot;
 
+
+import com.example.articleregisterationwithspringboot.exception.InValidDataException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ArticleRegisterationWithSpringBootApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InValidDataException {
         SpringApplication.run(ArticleRegisterationWithSpringBootApplication.class, args);
     }
 
