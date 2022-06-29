2434a6851160af718914c5d3a4431641936b10b9
neda <bashiri.neda72@gmail.com>
2022-06-29 12:30:05 +0430

    implemented ServiceImpl for entities

--- src/main/java/com/example/articleregisterationwithspringboot/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/example/articleregisterationwithspringboot/service/impl/ArticleServiceImpl.java
@@ -5,14 +5,18 @@ import com.example.articleregisterationwithspringboot.domains.Article;
 import com.example.articleregisterationwithspringboot.repository.ArticleRepository;
 import com.example.articleregisterationwithspringboot.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class ArticleServiceImpl extends BaseServiceImpl<Article, Long, ArticleRepository> implements ArticleService {
 
     @Autowired
+    @Qualifier(value = "articleRepository")
     ArticleRepository articleRepository;
 
     @Override
