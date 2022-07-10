4c5014b41fdd811a28a2695178bee48ee9f121dc
neda <bashiri.neda72@gmail.com>
2022-07-11 00:57:48 +0430

    implemented RepositoryImpl For entities

--- src/main/java/com/example/articleregisterationwithspringboot/repository/impl/ArticleRepositoryImpl.java
+++ src/main/java/com/example/articleregisterationwithspringboot/repository/impl/ArticleRepositoryImpl.java
@@ -2,19 +2,17 @@ package com.example.articleregisterationwithspringboot.repository.impl;
 
 import com.example.articleregisterationwithspringboot.base.repository.impl.BaseRepositoryImpl;
 import com.example.articleregisterationwithspringboot.domains.Article;
+import com.example.articleregisterationwithspringboot.domains.User;
 import com.example.articleregisterationwithspringboot.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 import java.util.List;
 
 
 @Repository("articleRepository")
-public class ArticleRepositoryImpl extends BaseRepositoryImpl<Article,Long> implements ArticleRepository {
+public class ArticleRepositoryImpl extends BaseRepositoryImpl<Article, Long> implements ArticleRepository {
 
     @PersistenceContext
     EntityManager entityManager;
@@ -29,14 +27,22 @@ public class ArticleRepositoryImpl extends BaseRepositoryImpl<Article,Long> impl
         } catch (NoResultException e) {
             System.out.println(e.getMessage());
         }
-
         return article;
     }
 
     @Override
     public List<Article> findPublishedArticles() {
-        TypedQuery<Article> query = entityManager.createQuery("select o from Article o where o.isPublished=:x",Article.class);
-        query.setParameter("x",true);
+        TypedQuery<Article> query = entityManager.createQuery("select o from Article o where o.published=:x", Article.class);
+        query.setParameter("x", true);
         return query.getResultList();
     }
+
+    @Override
+    public List<Article> findByUserName(String userName) {
+        TypedQuery<Article> query = entityManager.createQuery("SELECT e FROM Article e WHERE e.user.userName = :x", Article.class);
+        query.setParameter("x", userName);
+        return query.getResultList();
+    }
+
+
 }
